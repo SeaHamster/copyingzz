7b8a369539db82b9845365f72036c08b7ddbe365
SeaHamster <2221380662@qq.com>
2020-08-07 14:28:21 +0800

    --admin add 2020-08-07 搭建项目

---
+++ blogs/src/main/java/top/copying/blogs/CopyingBlogsApplication.java
@@ -0,0 +1,15 @@
+package top.copying.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Copying
+ * @date 2020-08-07
+ */
+@SpringBootApplication
+public class CopyingBlogsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CopyingBlogsApplication.class);
+    }
+}
